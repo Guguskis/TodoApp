b1969ff59a753c6e4db431066376e55728609ca4
Matas Liutikas <matasliutikas@gmail.com>
2019-12-17 20:12:52 +0200

    implemented company registration

--- src/main/java/todoapp/model/Company.java
+++ src/main/java/todoapp/model/Company.java
@@ -3,24 +3,32 @@ package main.java.todoapp.model;
 public class Company extends User {
 
     private String name;
-    private String contactPerson;
+    private String contactPersonPhone;
 
-    public Company(String name, String contactPerson, String username, String password) {
+    public Company(String name, String contactPersonPhone, String username, String password) {
         super(username, password);
         this.name = name;
-        this.contactPerson = contactPerson;
+        this.contactPersonPhone = contactPersonPhone;
     }
 
     @Override
     public String toString() {
-        return "Company: \"" + name + "\", contact person: " + contactPerson + ".";
+        return "Company: \"" + name + "\", contact person: " + contactPersonPhone + ".";
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setContactPerson(String contactPerson) {
-        this.contactPerson = contactPerson;
+    public String getContactPersonPhone() {
+        return contactPersonPhone;
+    }
+
+    public void setContactPersonPhone(String contactPersonPhone) {
+        this.contactPersonPhone = contactPersonPhone;
     }
 }
--- src/main/java/todoapp/repository/PersonConnection.java
+++ src/main/java/todoapp/repository/PersonConnection.java
@@ -20,6 +20,10 @@ import java.util.Map;
 import static java.net.http.HttpRequest.newBuilder;
 import static java.net.http.HttpResponse.BodyHandlers;
 
+/*
+* example
+* https://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
+* */
 public class PersonConnection {
     private final String BASE_URL = "http://localhost:8082/api/user/";
 
@@ -29,9 +33,9 @@ public class PersonConnection {
 
     public static void main(String[] args) throws Exception {
 
-        PersonConnection obj = new PersonConnection();
-        Person person = new Person("Matas", "Liutikas", "", "", "Matukulis", "matas");
-        obj.register(person);
+        PersonConnection connection = new PersonConnection();
+        Company company = new Company("MatasTM", "555",  "Matukuliszzz", "matas");
+        connection.register(company);
         System.out.println("Success??");
     }
 
@@ -71,65 +75,18 @@ public class PersonConnection {
         }
     }
 
-    public void register(Company company) {
+    public void register(Company company) throws JSONException, IOException, InterruptedException, RegistrationFailedException {
+        JSONObject companyJson = new JSONObject();
+        companyJson.put("name", company.getName());
+        companyJson.put("contactPersonPhone", company.getContactPersonPhone());
+        companyJson.put("username", company.getUsername());
+        companyJson.put("password", company.getPassword());
 
-    }
-
-    private void sendGet() throws Exception {
-
-        HttpRequest request = newBuilder()
-                .GET()
-                .uri(URI.create("https://httpbin.org/get"))
-                .setHeader("User-Agent", "Java 11 HttpClient Bot")
-                .build();
-
+        HttpRequest request = createPostRequest(companyJson, "/company");
         HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
 
-        // print status code
-        System.out.println(response.statusCode());
-
-        // print response body
-        System.out.println(response.body());
-
-    }
-
-    private void sendPost() throws Exception {
-
-        // form parameters
-        Map<Object, Object> data = new HashMap<>();
-        data.put("username", "abc");
-        data.put("password", "123");
-        data.put("custom", "secret");
-        data.put("ts", System.currentTimeMillis());
-
-        HttpRequest request = newBuilder()
-                .POST(buildFormDataFromMap(data))
-                .uri(URI.create("https://httpbin.org/post"))
-                .setHeader("User-Agent", "Java 11 HttpClient Bot") // add request header
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .build();
-
-        HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
-
-        // print status code
-        System.out.println(response.statusCode());
-
-        // print response body
-        System.out.println(response.body());
-
-    }
-
-    private static HttpRequest.BodyPublisher buildFormDataFromMap(Map<Object, Object> data) {
-        var builder = new StringBuilder();
-        for (Map.Entry<Object, Object> entry : data.entrySet()) {
-            if (builder.length() > 0) {
-                builder.append("&");
-            }
-            builder.append(URLEncoder.encode(entry.getKey().toString(), StandardCharsets.UTF_8));
-            builder.append("=");
-            builder.append(URLEncoder.encode(entry.getValue().toString(), StandardCharsets.UTF_8));
+        if (response.statusCode() != 201) {
+            throw new RegistrationFailedException(response.body());
         }
-        System.out.println(builder.toString());
-        return HttpRequest.BodyPublishers.ofString(builder.toString());
     }
 }
