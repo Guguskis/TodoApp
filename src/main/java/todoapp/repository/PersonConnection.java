c0efa5f1d718fa798fb6becf57d0c488b01035bc
Matas Liutikas <matasliutikas@gmail.com>
2019-12-17 20:19:37 +0200

    refactored

--- src/main/java/todoapp/repository/PersonConnection.java
+++ src/main/java/todoapp/repository/PersonConnection.java
@@ -9,21 +9,17 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
 import static java.net.http.HttpRequest.newBuilder;
 import static java.net.http.HttpResponse.BodyHandlers;
 
 /*
-* example
-* https://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
-* */
+ * example
+ * https://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
+ * */
 public class PersonConnection {
     private final String BASE_URL = "http://localhost:8082/api/user/";
 
@@ -34,20 +30,48 @@ public class PersonConnection {
     public static void main(String[] args) throws Exception {
 
         PersonConnection connection = new PersonConnection();
-        Company company = new Company("MatasTM", "555",  "Matukuliszzz", "matas");
+        Company company = new Company("MatasTM", "555", "Matukuliszzz", "matas");
         connection.register(company);
         System.out.println("Success??");
     }
 
-    public boolean login(User user) throws IOException, InterruptedException, JSONException {
-        JSONObject userJson = new JSONObject();
-        userJson.put("username", user.getUsername());
-        userJson.put("password", user.getPassword());
-
-        HttpRequest request = createPostRequest(userJson, "/verify");
-        HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
-
+    public boolean login(User user) throws IOException, InterruptedException {
+        HttpRequest request = createPostRequest(getUserJson(user), "/verify");
+        HttpResponse<String> response = send(request);
         return response.body().equals("true");
+    }
+
+    public void register(Company company) throws IOException, InterruptedException, RegistrationFailedException {
+        HttpRequest request = createPostRequest(getCompanyJson(company), "/company");
+        HttpResponse<String> response = send(request);
+
+        if (response.statusCode() != 201) {
+            throw new RegistrationFailedException(response.body());
+        }
+    }
+
+    public void register(Person person) throws RegistrationFailedException, IOException, InterruptedException {
+        HttpRequest request = createPostRequest(getPersonJson(person), "/person");
+        HttpResponse<String> response = send(request);
+
+        if (response.statusCode() != 201) {
+            throw new RegistrationFailedException(response.body());
+        }
+    }
+
+    private HttpResponse<String> send(HttpRequest request) throws IOException, InterruptedException {
+        return httpClient.send(request, BodyHandlers.ofString());
+    }
+
+    private JSONObject getUserJson(User user) {
+        JSONObject userJson = new JSONObject();
+        try {
+            userJson.put("username", user.getUsername());
+            userJson.put("password", user.getPassword());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return userJson;
     }
 
     private HttpRequest createPostRequest(JSONObject body, String endpoint) {
@@ -58,35 +82,32 @@ public class PersonConnection {
                 .build();
     }
 
-    public void register(Person person) throws JSONException, RegistrationFailedException, IOException, InterruptedException {
+    private JSONObject getPersonJson(Person person) {
         JSONObject personJson = new JSONObject();
-        personJson.put("firstName", person.getFirstName());
-        personJson.put("lastName", person.getLastName());
-        personJson.put("phone", person.getPhone());
-        personJson.put("email", person.getEmail());
-        personJson.put("username", person.getUsername());
-        personJson.put("password", person.getPassword());
-
-        HttpRequest request = createPostRequest(personJson, "/person");
-        HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
-
-        if (response.statusCode() != 201) {
-            throw new RegistrationFailedException(response.body());
+        try {
+            personJson.put("firstName", person.getFirstName());
+            personJson.put("lastName", person.getLastName());
+            personJson.put("phone", person.getPhone());
+            personJson.put("email", person.getEmail());
+            personJson.put("username", person.getUsername());
+            personJson.put("password", person.getPassword());
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+        return personJson;
     }
 
-    public void register(Company company) throws JSONException, IOException, InterruptedException, RegistrationFailedException {
-        JSONObject companyJson = new JSONObject();
-        companyJson.put("name", company.getName());
-        companyJson.put("contactPersonPhone", company.getContactPersonPhone());
-        companyJson.put("username", company.getUsername());
-        companyJson.put("password", company.getPassword());
-
-        HttpRequest request = createPostRequest(companyJson, "/company");
-        HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
-
-        if (response.statusCode() != 201) {
-            throw new RegistrationFailedException(response.body());
+    private JSONObject getCompanyJson(Company company) {
+        JSONObject companyJson = null;
+        try {
+            companyJson = new JSONObject();
+            companyJson.put("name", company.getName());
+            companyJson.put("contactPersonPhone", company.getContactPersonPhone());
+            companyJson.put("username", company.getUsername());
+            companyJson.put("password", company.getPassword());
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+        return companyJson;
     }
 }
